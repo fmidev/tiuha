1e4bc3e7a5dcf89763323f08f168e17bb2457484
Tero Keinänen <tero.keinanen@reaktor.com>
2021-09-16 12:31:25 +0300

    Add Geotools datastore factory for AWS Keyspaces

--- measurement-api/run.sh
+++ measurement-api/run.sh
@@ -5,7 +5,9 @@ source "$( dirname "${BASH_SOURCE[0]}" )/../scripts/common-functions.sh"
 function main {
   cd "$repo/measurement-api"
   mvn package
-  java -jar "$repo/measurement-api/target/measurement-api-1.0-SNAPSHOT-jar-with-dependencies.jar"
+  java -jar \
+    -Djavax.net.ssl.trustStore=./cassandra_truststore.jks -Djavax.net.ssl.trustStorePassword=amazon \
+    "$repo/measurement-api/target/measurement-api-1.0-SNAPSHOT-jar-with-dependencies.jar"
 }
 
 main "$@"
--- measurement-api/src/main/kotlin/fi/fmi/tiuha/Main.kt
+++ measurement-api/src/main/kotlin/fi/fmi/tiuha/Main.kt
@@ -1,5 +1,15 @@
 package fi.fmi.tiuha
 
+import org.geotools.data.DataStoreFinder
+
 fun main(args: Array<String>) {
-    print("Hello, world")
+    val dataStore = DataStoreFinder.getDataStore(mapOf(
+        "keyspaces.keyspaceName" to "measurements",
+        "keyspaces.catalog" to "testcatalog",
+        "keyspaces.region" to "eu-west-1"
+    ))
+
+    dataStore.getSchema("gdelt-quickstart").attributeDescriptors.forEach {
+            attr -> println(attr.name)
+    }
 }
