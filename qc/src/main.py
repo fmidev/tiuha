901278fbf994f0d018cca6bd4144b4ee80601871
Tero Keinänen <tero.keinanen@reaktor.com>
2021-10-07 16:58:48 +0300

    TIUHA-15: Add quality control flags to GeoJSON feature collection

--- qc/src/main.py
+++ qc/src/main.py
@@ -1,12 +1,8 @@
 import titanlib
 import numpy as np
+import json
+import numbers
 
-points = titanlib.Points(
-    [60, 60.1, 60.2],
-    [10, 10, 10],
-    [0, 0, 0]
-)
-values = [0, 1, -111]
 radius = [50000]
 num_min = [2]
 threshold = 2
@@ -16,10 +12,31 @@ min_std = 1
 num_iterations = 2
 
 def main():
-    print("Hello, world")
+    with open("/netatmo.geojson") as file:
+        featureCollection = json.JSONDecoder().decode(file.read())
+        features = featureCollection["features"]
 
-    flags = titanlib.buddy_check(points, values, radius, num_min,threshold, max_elev_diff, elev_gradient, min_std, num_iterations)
-    print(flags)
+        lats = []
+        lons = []
+        elevations = []
+        values = []
+
+        for feature in features:
+            lon, lat, elev = feature["geometry"]["coordinates"]
+            lats.append(lat)
+            lons.append(lon)
+            elevations.append(elev)
+            values.append(feature["properties"]["result"])
+
+        points = titanlib.Points(lats, lons, elevations)
+        flags = titanlib.buddy_check(points, values, radius, num_min,threshold, max_elev_diff, elev_gradient, min_std, num_iterations)
+
+        for feature, flag in zip(features, flags):
+            feature["properties"]["qcFlag"] = flag.item()
+
+        print(json.JSONEncoder().encode(featureCollection))
+
+
 
 if __name__ == "__main__":
     main()
