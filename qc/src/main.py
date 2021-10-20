3e1505cdc328bdec2e76d0967497aea2c2b5bbd9
Tero Keinänen <tero.keinanen@reaktor.com>
2021-10-20 13:58:43 +0300

    TIUHA-17: Write quality controlled geojson to S3

--- qc/src/main.py
+++ qc/src/main.py
@@ -16,35 +16,41 @@ num_iterations = 2
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--bucket', required=True)
-    parser.add_argument('--key', required=True)
+    parser.add_argument('--prefix', required=True)
+    parser.add_argument('--filename', required=True)
     args = parser.parse_args()
 
+    input_filename = 'temp.geojson.gz'
     s3 = boto3.client('s3')
-    s3.download_file(args.bucket, args.key, 'temp.geojson.gz')
+    s3.download_file(args.bucket, args.prefix + args.filename, input_filename)
 
-    with gzip.open("temp.geojson.gz", mode="rt") as file:
-        featureCollection = json.JSONDecoder().decode(file.read())
-        features = featureCollection["features"]
+    output_filename = 'result.geojson.gz'
+    with gzip.open(input_filename, mode="rt") as input_file:
+        with gzip.open(output_filename, mode="wt") as output_file:
+            featureCollection = json.JSONDecoder().decode(input_file.read())
+            features = featureCollection["features"]
 
-        lats = []
-        lons = []
-        elevations = []
-        values = []
+            lats = []
+            lons = []
+            elevations = []
+            values = []
 
-        for feature in features:
-            lon, lat, elev = feature["geometry"]["coordinates"]
-            lats.append(lat)
-            lons.append(lon)
-            elevations.append(elev)
-            values.append(feature["properties"]["result"])
+            for feature in features:
+                lon, lat, elev = feature["geometry"]["coordinates"]
+                lats.append(lat)
+                lons.append(lon)
+                elevations.append(elev)
+                values.append(feature["properties"]["result"])
 
-        points = titanlib.Points(lats, lons, elevations)
-        flags = titanlib.buddy_check(points, values, radius, num_min,threshold, max_elev_diff, elev_gradient, min_std, num_iterations)
+            points = titanlib.Points(lats, lons, elevations)
+            flags = titanlib.buddy_check(points, values, radius, num_min,threshold, max_elev_diff, elev_gradient, min_std, num_iterations)
 
-        for feature, flag in zip(features, flags):
-            feature["properties"]["qcFlag"] = flag.item()
+            for feature, flag in zip(features, flags):
+                feature["properties"]["qcFlag"] = flag.item()
 
-        print(json.JSONEncoder().encode(featureCollection))
+            output_file.write(json.JSONEncoder().encode(featureCollection))
+
+    s3.upload_file(output_filename, args.bucket, args.prefix + "qc-" + args.filename)
 
 
 
